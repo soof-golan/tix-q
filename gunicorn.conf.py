f8570c4d3c7c9c11463ac4d937e4ae20dbee26dc
Soof Golan <soofgolan@gmail.com>
2023-05-26 23:40:57 +0300

    add num workers env var

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,12 @@
-import multiprocessing
 import os
 
+# Cloud Run cpu count lies, so we pass the explicit value in an env var
+NUM_WORKERS = int(os.environ.get('NUM_WORKERS', 1))
+
+# Cloud Run sets PORT automatically
 port = int(os.environ.get("PORT", 8000))
 
 wsgi_app = "main:app"
 bind = f'0.0.0.0:{port}'
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = NUM_WORKERS
 worker_class = 'uvicorn.workers.UvicornWorker'
