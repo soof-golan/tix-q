ce96273bd4acc32336c1e08c733ee2df3fcaa304
Soof Golan <soofgolan@gmail.com>
2023-06-03 02:17:21 +0300

    make backend code more idiomatic about state management

--- server/main.py
+++ server/main.py
@@ -1,5 +1,8 @@
+import contextlib
+import datetime
 import os
-from typing import Annotated
+import typing
+from typing import Annotated, TypedDict
 
 import fastapi
 from asyncache import cached
@@ -13,7 +16,29 @@ from prisma import Prisma, enums, errors, models, register
 
 TTL_FIVE_MINUTES = 60 * 5
 
-app = fastapi.FastAPI()
+
+class State(TypedDict):
+    http_client: httpx.AsyncClient
+    turnstile_secret: str
+    db: Prisma
+
+
+@contextlib.asynccontextmanager
+async def lifespan(_app: fastapi.FastAPI) -> typing.AsyncIterator[State]:
+    turnstile_secret = os.environ.get("TURNSTILE_SECRET", "secret")
+    db = Prisma()
+    register(db)
+    await db.connect()
+    async with httpx.AsyncClient() as client:
+        yield {
+            "http_client": client,
+            "turnstile_secret": turnstile_secret,
+            "db": db,
+        }
+    await db.disconnect()
+
+
+app = fastapi.FastAPI(lifespan=lifespan)
 
 # TODO: Are these the right origins?
 origins = [
@@ -30,62 +55,61 @@ app.add_middleware(
     allow_headers=["X-Turnstile-Token"],
 )
 
-
-@app.on_event("startup")
-async def start():
-    """
-    Cold start
-    """
-    client = httpx.AsyncClient(
-        base_url="https://challenges.cloudflare.com/turnstile/v0/siteverify", timeout=5
-    )
-    app.turnstile_secret = os.environ.get("TURNSTILE_SECRET", "secret")
-    app.client = client
-    db = Prisma()
-    register(db)
-    await db.connect()
-    app.db = db
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    await app.db.disconnect()
+PLAY_NICE_RESPONSE = """
+Hey {name}!,
+I know going to this event is important to you, but please don't hack me.
+I'm running this as a free service for the community, and I'm doing my best to make sure everyone gets a fair chance.
+If you think this is a mistake, please contact me.
+If you're running a bot, please stop.
+Toodles ❤️
+Soof
+"""
 
 
 class TurnstileOutcome(BaseModel):
     success: bool
-    challenge_ts: str | None = None  # Verify people don't submit too early
+    challenge_ts: datetime.datetime | None = None  # Verify people don't submit too early
     data: dict | None = None
 
 
-async def validate_turnstile(token: str | None) -> TurnstileOutcome:
+async def validate_turnstile(request: fastapi.Request, token: str | None) -> TurnstileOutcome:
     """
     Validate the turnstile token
     As documented in https://developers.cloudflare.com/turnstile/get-started/server-side-validation/
     """
-    with app.client as client:
-        response = await client.get(
-            "/",
-            data={
-                "secret": app.turnstile_secret,  # Our secret
-                "response": token,  # Came from the client
-            },
+    client = request.state.http_client
+    turnstile_secret = request.state.turnstile_secret
+    # async with httpx.AsyncClient(timeout=5) as client:
+    response = await client.post(
+        "https://challenges.cloudflare.com/turnstile/v0/siteverify/",
+        timeout=5,
+        data={
+            "secret": turnstile_secret,  # Our secret
+            "response": token,  # Came from the client
+        },
+    )
+    response.raise_for_status()
+    data = response.json()
+    challenge_ts = data.get("challenge_ts")
+    if challenge_ts is None:
+        raise fastapi.HTTPException(
+            status_code=500, detail="Internal server error (missing challenge_ts)" + PLAY_NICE_RESPONSE.format(name="maybe bot?"),
         )
-        response.raise_for_status()
-        data = response.json()
-        challenge_ts = data.get("challenge_ts")
-        if challenge_ts is None:
-            raise fastapi.HTTPException(
-                status_code=500, detail="Internal server error (missing challenge_ts)"
-            )
-        success = data.get("success")
-        if success is None:
-            raise fastapi.HTTPException(
-                status_code=500, detail="Internal server error (missing success)"
-            )
-        return TurnstileOutcome(
-            success=data["success"], challenge_ts=data["challenge_ts"], data=data
+    success = data.get("success")
+    if success is None:
+        raise fastapi.HTTPException(
+            status_code=500, detail="Internal server error (missing success)" + PLAY_NICE_RESPONSE.format(name="maybe bot?"),
         )
+    try:
+        timestamp = datetime.datetime.fromisoformat(challenge_ts)
+    except ValueError:
+        raise fastapi.HTTPException(
+            status_code=500,
+            detail="Internal server error (invalid challenge_ts)" + PLAY_NICE_RESPONSE.format(name="maybe bot?"),
+        )
+    return TurnstileOutcome(
+        success=data["success"], challenge_ts=timestamp, data=data
+    )
 
 
 class PrticipantRegisterRequest(BaseModel):
@@ -114,12 +138,13 @@ async def fetch_waiting_room(waiting_room_id: str) -> models.WaitingRoom | None:
     return await models.WaitingRoom.prisma().find_unique(where={"id": waiting_room_id})
 
 
-@app.post("/")
+@app.post("/register")
 async def create_participant(
-    data: PrticipantRegisterRequest,
-    x_turnstile_token: Annotated[str | None, Header()] = None,
+        request: fastapi.Request,
+        data: PrticipantRegisterRequest,
+        x_turnstile_token: Annotated[str | None, Header()] = None,
 ) -> ParticipantRegisterResponse:
-    outcome = await validate_turnstile(x_turnstile_token)
+    outcome = await validate_turnstile(request, x_turnstile_token)
 
     # This user might be a bot. We don't allow bots to register.
     if not outcome.success:
@@ -151,30 +176,25 @@ async def create_participant(
             }
         )
     except errors.ForeignKeyViolationError:
-        raise fastapi.HTTPException(status_code=400, detail="Invalid waiting room ID")
+        raise fastapi.HTTPException(status_code=400, detail="Invalid waiting room ID" + PLAY_NICE_RESPONSE.format(name=data.legalName))
     except errors.RecordNotFoundError:
         raise fastapi.HTTPException(
-            status_code=500, detail="Internal server error (table not found)"
+            status_code=500, detail="Internal server error (table not found)" + PLAY_NICE_RESPONSE.format(name=data.legalName)
         )
     except errors.DataError as e:
         raise fastapi.HTTPException(
-            status_code=500, detail=repr(e.data) or "Invalid data"
+            status_code=500, detail=repr(e.data) or "Invalid data" + PLAY_NICE_RESPONSE.format(name=data.legalName)
         )
     except errors.ClientNotConnectedError:
         raise fastapi.HTTPException(
-            status_code=500, detail="Internal server error (db not connected)"
+            status_code=500, detail="Internal server error (db not connected)" + PLAY_NICE_RESPONSE.format(name=data.legalName)
         )
 
     # Someone is trying to register too early
     if outcome.challenge_ts < room.opensAt:
         raise fastapi.HTTPException(
             status_code=400,
-            detail=f"""
-            Hey {data.legalName}!, 
-            it's too early to register, please don't hack me. 
-            I'm running this as a volunteer, at DDoSing me will cost me money :(
-            This request was recored in the database, don't do it again.
-            """,
+            detail=f"Too early to register." + PLAY_NICE_RESPONSE.format(name=data.legalName) + "\nbtw this request was recorded."
         )
 
     return result.dict()
