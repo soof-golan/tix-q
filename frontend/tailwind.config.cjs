879c9c979ab5e66250c2aa90c5ef1f76a3043bf2
Soof Golan <soofgolan@gmail.com>
2023-06-04 00:20:17 +0300

    Add radix-ui

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,8 +1,16 @@
+const {blackA, violet, mauve} = require('@radix-ui/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...blackA,
+        ...violet,
+        ...mauve,
+      }
+    },
   },
   plugins: [],
 };
