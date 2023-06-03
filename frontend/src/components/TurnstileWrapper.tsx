f0a4e6efe73506e512cbf8830404310d6361d9f0
Soof Golan <soofgolan@gmail.com>
2023-06-03 19:05:28 +0300

    create TurnstileWrapper

---
+++ frontend/src/components/TurnstileWrapper.tsx
@@ -0,0 +1,27 @@
+import Turnstile from "react-turnstile";
+import {useRef} from "react";
+
+export function TurnstileWrapper(props: {
+  onLoad: () => void,
+  onVerify: (token: string) => void,
+  onError: (error: any) => void
+}) {
+  const siteKey = import.meta.env.PUBLIC_TURNSTILE_SITEKEY;
+  return <>
+    <Turnstile
+      sitekey={siteKey}
+      action="register"
+      size="normal"
+      retry="auto"
+      onLoad={props.onLoad}
+      onVerify={props.onVerify}
+      onError={props.onError}
+      onExpire={props.onLoad}
+      retryInterval={1000}
+      refreshExpired="auto"
+      execution="render"
+      onTimeout={props.onLoad}
+      appearance="always"
+    />
+  </>;
+}
