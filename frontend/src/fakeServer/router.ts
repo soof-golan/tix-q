eeb5ed8832dde8f1a8b6b7012f057d648205a0bb
Soof Golan <soofgolan@gmail.com>
2023-06-03 19:05:06 +0300

    extract procedure types

--- frontend/src/fakeServer/router.ts
+++ frontend/src/fakeServer/router.ts
@@ -1,38 +1,12 @@
-import * as trpc from '@trpc/server';
 import {publicProcedure, router} from './trpc';
-import {z} from "zod";
+import {RegisterOutput, registerInputSchema} from "../types/RegisterProcedure";
 
 const appRouter = router({
-  register: publicProcedure.input(z.object({
-    email: z.string().email(),
-    legalName: z.string(),
-    idNumber: z.string(),
-    idType: z.enum(['PASSPORT', 'ID_CARD']),
-    waitingRoomId: z.string().uuid(),
-    phoneNumber: z.string(),
-    // TODO: turnstile token
-  })).mutation(() => {
-    /*
-    * id: UUID4
-    legalName: str
-    email: EmailStr
-    phoneNumber: str
-    idNumber: str
-    idType: enums.IdType
-    waitingRoomId: str
-    * */
-    return {
-      id: '123',
-      legalName: 'John Doe',
-      email: 'example@example.com',
-      phoneNumber: '+1234567890',
-      idNumber: '1234567890',
-      idType: 'PASSPORT',
-      waitingRoomId: '123',
-    }
+  register: publicProcedure.input(registerInputSchema).mutation(async (): Promise<RegisterOutput> => {
+    return {} as RegisterOutput;
   })
 });
 
-// Export only the type of a router!
+// Export only the "type" of a router!
 // This prevents us from importing server code on the client.
 export type AppRouter = typeof appRouter;
---
+++ frontend/src/types/RegisterProcedure.ts
@@ -0,0 +1,20 @@
+import {z} from "zod";
+
+export let registerInputSchema = z.object({
+  email: z.string().email(),
+  legalName: z.string(),
+  idNumber: z.string(),
+  idType: z.enum(['PASSPORT', 'ID_CARD']),
+  waitingRoomId: z.string().uuid(),
+  phoneNumber: z.string(),
+});
+export type RegisterInput = z.infer<typeof registerInputSchema>;
+export type RegisterOutput = {
+  id: string,
+  legalName: string,
+  email: string,
+  phoneNumber: string,
+  idNumber: string,
+  idType: 'PASSPORT' | 'ID_CARD',
+  waitingRoomId: string,
+}
