f01a9e158f206c660aa551c5af170ff2822fc2dc
Soof Golan <soofgolan@gmail.com>
2023-06-04 18:51:42 +0300

    use `global`

--- frontend/src/db.ts
+++ frontend/src/db.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
+const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined;
 };
 
