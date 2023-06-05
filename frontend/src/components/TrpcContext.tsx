27f54665e941d4ef35daee3e490a19d9e6581a13
Soof Golan <soofgolan@gmail.com>
2023-06-05 15:30:22 +0300

    Add firebase Authorization header to trpc

--- frontend/src/components/TrpcContext.tsx
+++ frontend/src/components/TrpcContext.tsx
@@ -2,8 +2,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpLink } from "@trpc/client";
 import { PropsWithChildren, useState } from "react";
 import { trpc } from "../utils/trpc";
+import { useAuth, useSigninCheck, useUser } from "reactfire";
 
 export default function TrpcContext({ children }: PropsWithChildren) {
+  const signInCheck = useSigninCheck({ forceRefresh: true });
+  const auth = useAuth();
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
@@ -11,8 +14,11 @@ export default function TrpcContext({ children }: PropsWithChildren) {
         httpLink({
           url: import.meta.env.PUBLIC_SERVER_URL,
           headers: async () => {
+            await signInCheck.firstValuePromise;
+            const token = await auth.currentUser?.getIdToken?.();
             return {
               "X-CSRF-Token": "csrf-token",
+              ...(token ? { Authorization: `Bearer ${token}` } : {}),
             };
           },
           fetch(url, options) {
