216deea1442ead672209d8908b940b4752abb3f4
Soof Golan <soofgolan@gmail.com>
2023-06-05 18:11:09 +0300

    Add markdown tutorial content

---
+++ frontend/src/constants.ts
@@ -0,0 +1,31 @@
+export const markdownTips = `
+# This is an h1
+## This is an h2
+### This is an h3
+#### This is an h4
+##### This is an h5
+###### This is an h6
+
+**This text is in bold.**
+
+_This text is with italics_
+
+ואפשר גם בעברית! (וזה מיושר בערך כמו בוואצאפ)
+
+# כולל כותרות
+
+ואפשר גם לשים תמונות
+
+![alt text goes here](https://placekitten.com/200/200)
+
+כי לרדת שורה צריך שני אנטרים.
+אחרת זה ממשיך באותה פסקה
+
+עוד טיפים יש [כאן](https://learnxinyminutes.com/docs/markdown/)
+
+More tips [here](https://learnxinyminutes.com/docs/markdown/) 
+
+בהצלחה!
+`;
+
+export const markdownTipsTitle = "Main Title - כותרת ראשית";
