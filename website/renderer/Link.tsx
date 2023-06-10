9a8b914a943deb43c161c00a3012ea86cc99930a
Soof Golan <soofgolan@gmail.com>
2023-06-10 14:19:50 +0300

    Add onClick to Link component

--- website/renderer/Link.tsx
+++ website/renderer/Link.tsx
@@ -1,9 +1,20 @@
-import { usePageContext } from './usePageContext'
+import { usePageContext } from "./usePageContext";
+import React from "react";
 
-export { Link }
+export { Link };
 
-function Link(props: { href?: string; className?: string; children: React.ReactNode }) {
-  const pageContext = usePageContext()
-  const className = [props.className, pageContext.urlPathname === props.href && 'is-active'].filter(Boolean).join(' ')
-  return <a {...props} className={className} />
+function Link(props: {
+  href?: string;
+  className?: string;
+  onClick?: React.AnchorHTMLAttributes<HTMLAnchorElement>["onClick"];
+  children: React.ReactNode;
+}) {
+  const pageContext = usePageContext();
+  const className = [
+    props.className,
+    pageContext.urlPathname === props.href && "is-active",
+  ]
+    .filter(Boolean)
+    .join(" ");
+  return <a {...props} className={className} />;
 }
