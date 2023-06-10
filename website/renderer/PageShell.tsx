ec9259d217edecde5eed0d1bf02068aeaf61685b
Soof Golan <soofgolan@gmail.com>
2023-06-10 14:20:26 +0300

    Update page shell and layout

--- website/renderer/PageShell.tsx
+++ website/renderer/PageShell.tsx
@@ -1,77 +1,45 @@
-import React from 'react'
-import logo from './logo.svg'
-import { PageContextProvider } from './usePageContext'
-import type { PageContext } from './types'
-import './PageShell.css'
-import { Link } from './Link'
+import React from "react";
+import logo from "./logo.svg";
+import { PageContextProvider } from "./usePageContext";
+import type { PageContext } from "./types";
+import "./PageShell.css";
+import { Link } from "./Link";
+import Header from "../components/Header";
+import { FirebaseContext } from "../components/FirebaseContext";
 
-export { PageShell }
+export { PageShell };
 
-function PageShell({ children, pageContext }: { children: React.ReactNode; pageContext: PageContext }) {
+function PageShell({
+  children,
+  pageContext,
+}: {
+  children: React.ReactNode;
+  pageContext: PageContext;
+}) {
   return (
     <React.StrictMode>
       <PageContextProvider pageContext={pageContext}>
-        <Layout>
-          <Sidebar>
-            <Logo />
-            <Link className="navitem" href="/">
-              Home
-            </Link>
-            <Link className="navitem" href="/about">
-              About
-            </Link>
-          </Sidebar>
-          <Content>{children}</Content>
-        </Layout>
+        <FirebaseContext>
+          <Layout>
+            <Header title="Waiting Room" />
+            <Content>{children}</Content>
+          </Layout>
+        </FirebaseContext>
       </PageContextProvider>
     </React.StrictMode>
-  )
+  );
 }
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div
-      style={{
-        display: 'flex',
-        maxWidth: 900,
-        margin: 'auto'
-      }}
-    >
+    <div className="min-h-screen w-full bg-gradient-to-l from-purple-600 to-indigo-600">
       {children}
     </div>
-  )
-}
-
-function Sidebar({ children }: { children: React.ReactNode }) {
-  return (
-    <div
-      style={{
-        padding: 20,
-        flexShrink: 0,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        lineHeight: '1.8em'
-      }}
-    >
-      {children}
-    </div>
-  )
+  );
 }
 
 function Content({ children }: { children: React.ReactNode }) {
-  return (
-    <div
-      style={{
-        padding: 20,
-        paddingBottom: 50,
-        borderLeft: '2px solid #eee',
-        minHeight: '100vh'
-      }}
-    >
-      {children}
-    </div>
-  )
+  return <main>{children}</main>;
 }
 
 function Logo() {
@@ -79,12 +47,12 @@ function Logo() {
     <div
       style={{
         marginTop: 20,
-        marginBottom: 10
+        marginBottom: 10,
       }}
     >
       <a href="/">
         <img src={logo} height={64} width={64} alt="logo" />
       </a>
     </div>
-  )
+  );
 }
