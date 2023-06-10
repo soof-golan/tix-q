4f0af3d91a8f5163f6694e10295e26e2d10e1c13
Soof Golan <soofgolan@gmail.com>
2023-06-11 00:27:44 +0300

    allow spa mode (maybe)

--- website/renderer/_default.page.client.tsx
+++ website/renderer/_default.page.client.tsx
@@ -1,19 +1,14 @@
 export { render };
+export const clientRouting = true;
+export const hydrationCanBeAborted = true;
 
 import { hydrateRoot, createRoot } from "react-dom/client";
 import { PageShell } from "./PageShell";
 import type { PageContextClient } from "./types";
 
-export const clientRouting = true;
-export const hydrationCanBeAborted = true;
-
-// This render() hook only supports SSR, see https://vite-plugin-ssr.com/render-modes for how to modify render() to support SPA
 async function render(pageContext: PageContextClient) {
   const { Page, pageProps } = pageContext;
-  if (!Page)
-    throw new Error(
-      "Client-side render() hook expects pageContext.Page to be defined"
-    );
+
   const page = (
     <PageShell pageContext={pageContext}>
       <Page {...pageProps} />
@@ -21,7 +16,7 @@ async function render(pageContext: PageContextClient) {
   );
   const root = document.getElementById("react-root");
   if (!root) throw new Error("DOM element #react-root not found");
-  if (root.innerHTML === "") {
+  if (root.innerHTML === "" || !pageContext.isHydration) {
     const reactRoot = createRoot(root);
     reactRoot.render(page);
   } else {
--- website/renderer/_default.page.server.tsx
+++ website/renderer/_default.page.server.tsx
@@ -2,6 +2,8 @@ export { render };
 // See https://vite-plugin-ssr.com/data-fetching
 export const passToClient = ["pageProps"];
 
+export const doNotPrerender = true;
+
 import ReactDOMServer from "react-dom/server";
 import { PageShell } from "./PageShell";
 import { escapeInject, dangerouslySkipEscape } from "vite-plugin-ssr/server";
@@ -10,14 +12,16 @@ import type { PageContextServer } from "./types";
 
 async function render(pageContext: PageContextServer) {
   const { Page, pageProps } = pageContext;
-  // This render() hook only supports SSR, see https://vite-plugin-ssr.com/render-modes for how to modify render() to support SPA
-  if (!Page)
-    throw new Error("My render() hook expects pageContext.Page to be defined");
-  const pageHtml = ReactDOMServer.renderToString(
-    <PageShell pageContext={pageContext}>
-      <Page {...pageProps} />
-    </PageShell>
-  );
+
+  const spaMode = !Page;
+
+  const pageHtml = spaMode
+    ? ""
+    : ReactDOMServer.renderToString(
+        <PageShell pageContext={pageContext}>
+          <Page {...pageProps} />
+        </PageShell>
+      );
 
   // See https://vite-plugin-ssr.com/head
   const { documentProps } = pageContext.exports;
