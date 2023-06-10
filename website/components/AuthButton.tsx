130c8ee5d566d194816b1d12dab3eff0081c85ed
Soof Golan <soofgolan@gmail.com>
2023-06-10 14:07:27 +0300

    copy auth button

---
+++ website/components/AuthButton.tsx
@@ -0,0 +1,54 @@
+import { useSigninCheck } from "reactfire";
+import { auth } from "../utils/firebase";
+import { FirebaseContext } from "./FirebaseContext";
+
+function AuthButton_() {
+  const signinCheck = useSigninCheck();
+  const loading = signinCheck.status === "loading";
+  const signedIn = !!signinCheck.data?.signedIn;
+  const buttonText = loading ? "Loading..." : signedIn ? "Sign out" : "Sign in";
+  if (loading)
+    return (
+      <>
+        <button
+          className="rounded bg-purple-600 px-4 py-2 text-sm font-bold text-white hover:bg-purple-700"
+          disabled
+        >
+          {buttonText}
+        </button>
+      </>
+    );
+  if (signedIn)
+    return (
+      <>
+        <a
+          href="/auth"
+          onClick={(event) => {
+            event.preventDefault();
+            auth.signOut();
+          }}
+        >
+          <button className="rounded bg-purple-600 px-4 py-2 text-sm font-bold text-white hover:bg-purple-700">
+            {buttonText}
+          </button>
+        </a>
+      </>
+    );
+  return (
+    <>
+      <a href="/auth">
+        <button className="rounded bg-purple-600 px-4 py-2 text-sm font-bold text-white hover:bg-purple-700">
+          {buttonText}
+        </button>
+      </a>
+    </>
+  );
+}
+
+export default function AuthButton() {
+  return (
+    <FirebaseContext>
+      <AuthButton_ />
+    </FirebaseContext>
+  );
+}
