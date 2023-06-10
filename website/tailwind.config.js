64b88cd441686f96a2b5299eef6b59a80e9b9a7c
Soof Golan <soofgolan@gmail.com>
2023-06-10 12:43:14 +0300

    Add tailwind

---
+++ website/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./renderer/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
