4dcc86b838c58467012fe41f84667e78dbcfe749
Soof Golan <soofgolan@gmail.com>
2023-06-12 12:23:30 +0300

    copy constants

---
+++ website/constants.ts
@@ -0,0 +1,37 @@
+export const markdownTips = `
+# Welcome to the waiting room editor!
+
+This code editor uses Markdown, a simple markup language that allows you to write text with some formatting.
+
+here's a quick guide:
+
+# This is an h1
+## This is an h2
+### This is an h3
+#### This is an h4
+##### This is an h5
+###### This is an h6
+
+**This text is in bold.**
+
+_This text is with italics_
+
+ואפשר גם בעברית! (וזה מיושר בערך כמו בוואצאפ)
+
+# כולל כותרות
+
+ואפשר גם לשים תמונות
+
+![alt text goes here](https://placekitten.com/200/200)
+
+כי לרדת שורה צריך שני אנטרים.
+אחרת זה ממשיך באותה פסקה
+
+עוד טיפים יש [כאן](https://learnxinyminutes.com/docs/markdown/)
+
+More tips [here](https://learnxinyminutes.com/docs/markdown/) 
+
+בהצלחה!
+`;
+
+export const markdownTipsTitle = "Main Title - כותרת ראשית";
