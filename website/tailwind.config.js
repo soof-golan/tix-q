928ce52b08c5f43df7846eef4741127f74a5c55c
Soof Golan <soofgolan@gmail.com>
2023-06-12 12:26:00 +0300

    add tailwind to components

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -3,10 +3,10 @@ export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./renderer/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
