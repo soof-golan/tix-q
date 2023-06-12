b0267c25bbd96769ceda4e1998c167fef614d2c6
Soof Golan <soofgolan@gmail.com>
2023-06-12 17:04:25 +0300

    migration before deployment

---
+++ prisma/migrations/20230612140249_cleanup_before_deployment/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Registrant" ALTER COLUMN "createdAt" SET DEFAULT now();
+
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "createdAt" SET DEFAULT now();
+
+-- AlterTable
+ALTER TABLE "WaitingRoom" ALTER COLUMN "createdAt" SET DEFAULT now();
