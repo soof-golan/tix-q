c8cfb2ead36366a6cb543af17494b650b07dec3f
Soof Golan <soofgolan@gmail.com>
2023-06-12 12:59:35 +0300

    Pass route params to client

--- website/renderer/_default.page.server.tsx
+++ website/renderer/_default.page.server.tsx
@@ -1,6 +1,6 @@
 export { render };
 // See https://vite-plugin-ssr.com/data-fetching
-export const passToClient = ["pageProps"];
+export const passToClient = ["pageProps", "routeParams"];
 
 export const doNotPrerender = true;
 
