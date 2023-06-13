542f64a0f37a9576ed38edd6b5c0913eee98cbdc
Soof Golan <soofgolan@gmail.com>
2023-06-13 13:21:20 +0300

    move publish button out of form

--- website/components/WaitingRoomEditor.tsx
+++ website/components/WaitingRoomEditor.tsx
@@ -120,19 +120,6 @@ export default function WaitingRoomEditor({ id }: WaitingRoomContentProps) {
             >
               {dirty ? <>Save</> : <>Saved</>}
             </button>
-            <button
-              disabled={roomQuery.data.published || dirty}
-              onClick={() => publishApi.mutate({ id })}
-              className="mr-2 mt-2 rounded bg-indigo-500 px-4 py-2 text-white hover:bg-indigo-700 disabled:cursor-not-allowed disabled:opacity-50"
-            >
-              {roomQuery.data.published ? (
-                <>Room Public 🚀</>
-              ) : dirty ? (
-                <>Save before publishing</>
-              ) : (
-                <>Publish (cannot be undone)</>
-              )}
-            </button>
           </div>
           <div className="border-t border-gray-200">
             <dl>
@@ -200,11 +187,29 @@ export default function WaitingRoomEditor({ id }: WaitingRoomContentProps) {
               </div>
             </dl>
           </div>
-          <div className="px-4 py-5 max-sm:flex-col sm:px-6">
+        </form>
+        <div className="flex justify-between px-4 py-5 max-sm:flex-col sm:px-6">
+          <div>
             <p>Preview in the card below:</p>
             <p>This is how the content will look like after publishing.</p>
           </div>
-        </form>
+          <button
+            type="button"
+            // disabled={roomQuery.data.published || dirty}
+            onClick={() => {
+              publishApi.mutate({ id });
+            }}
+            className="mr-2 mt-2 rounded bg-indigo-500 px-4 py-2 text-white hover:bg-indigo-700 disabled:cursor-not-allowed disabled:opacity-50"
+          >
+            {roomQuery.data.published ? (
+              <>Room Public 🚀</>
+            ) : dirty ? (
+              <>Save before publishing</>
+            ) : (
+              <>Publish (cannot be undone)</>
+            )}
+          </button>
+        </div>
       </div>
 
       <MarkdownCard title={liveTitle} content={liveMarkdown} />
