86f63370d2e676e2a18d28ac2ec2f386b7ba1df2
Soof Golan <soofgolan@gmail.com>
2023-06-13 10:38:45 +0300

    no default credentials

--- server/config.py
+++ server/config.py
@@ -8,7 +8,7 @@ class Config(BaseModel):
     Configuration for the backend
     """
 
-    firebase_credentials: str = os.environ.get("FIREBASE_CREDENTIALS", "firebase_credentials.json")
+    firebase_credentials: str = os.environ.get("FIREBASE_CREDENTIALS")
     """
     The path to the Firebase credentials file OR the contents of the file in JSON format
     """
