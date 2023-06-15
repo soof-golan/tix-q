43fa84a6386213fafd394c1600c66bb2089e9737
Soof Golan <soofgolan@gmail.com>
2023-06-15 14:43:10 +0300

    remove unused type helper

--- website/renderer/types.ts
+++ website/renderer/types.ts
@@ -28,23 +28,8 @@ type PageContextClient = PageContextBuiltInClient<Page> & PageContextCustom;
 
 type PageContext = PageContextClient | PageContextServer;
 
-// Recursively serialize all values in `T` to `string | number | null | undefined | boolean`.
-// Objects and arrays are serialized recursively.
-// Since PageProps are serialized, Dates are mapped to `string` type (in ISO format).
-type Ser<T> = {
-  [K in keyof T]: T[K] extends string | number | null | undefined | boolean
-    ? T[K]
-    : T[K] extends Date
-    ? string
-    : T[K] extends Array<infer U>
-    ? Array<Ser<U>>
-    : T[K] extends Record<string, infer U>
-    ? Record<string, Ser<U>>
-    : T[K];
-};
-
 export type inferProps<F> = F extends (...args: unknown[]) => Promise<infer T>
   ? T extends { pageContext: { pageProps: infer U } }
-    ? Ser<U>
+    ? U
     : never
   : never;
