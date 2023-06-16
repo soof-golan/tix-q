ef47b27945e4f9b5cc786d7f8610f6507bc9aa10
Soof Golan <soofgolan@gmail.com>
2023-06-16 13:10:01 +0300

    Add logger

--- server/constants.py
+++ server/constants.py
@@ -26,3 +26,24 @@ PROD_CORS_ORIGINS = [
     "https://waitingroom.soofgolan.com",
     "https://tix-q-api.soofgolan.com",
 ]
+log_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": "%(levelprefix)s %(asctime)s %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+    "handlers": {
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
+    },
+    "loggers": {
+        "tix-q": {"handlers": ["default"], "level": "INFO"},
+    },
+}
