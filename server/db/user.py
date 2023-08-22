1c59d2fe4de7fe08478dd5cb0033ab292581a61b
Soof Golan <soofgolan@gmail.com>
2023-08-22 19:55:39 +0300

    feat: use prisma for user fetching

--- server/db/user.py
+++ server/db/user.py
@@ -1,23 +1,46 @@
+import uuid
+
 from asyncache import cached
 from cachetools import TTLCache
-from prisma import models
+from pydantic import BaseModel
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from server.constants import TTL_FIVE_MINUTES
+from server.models import User as DbUser
 from server.types import FirebaseUser
 
 
-@cached(cache=TTLCache(maxsize=1024, ttl=TTL_FIVE_MINUTES))
-async def fetch_or_create_cached_user(user: FirebaseUser) -> models.User:
+class User(BaseModel):
+    id: uuid.UUID
+    firebase_uid: str
+    email: str
+
+
+async def fetch_or_create_cached_user(
+    session: AsyncSession, user: FirebaseUser
+) -> User:
     """
     Fetch or create a user by their Firebase UID
     This is cached for 5 minutes as user records are immutable
     """
-    return await models.User.prisma().upsert(where={"firebaseUid": user.identity}, data={
-        "create": {
-            "firebaseUid": user.identity,
-            "email": user.email
-        },
-        "update": {
-            "email": user.email
-        },
-    })
+
+    @cached(cache=TTLCache(maxsize=1024, ttl=TTL_FIVE_MINUTES))
+    async def _cached_fetch_or_create_user(_user: FirebaseUser) -> User:
+        async with session.begin_nested():
+            stmt = (
+                insert(DbUser)
+                .values(firebase_uid=_user.identity, email=_user.email)
+                .on_conflict_do_update(
+                    index_elements=[DbUser.firebase_uid],
+                    set_={DbUser.email: _user.email},
+                )
+                .returning(DbUser)
+            )
+            _result = (await session.execute(stmt)).scalar_one()
+            result = User(
+                id=_result.id, firebase_uid=_result.firebase_uid, email=_result.email
+            )
+        return result
+
+    return await _cached_fetch_or_create_user(user)
--- server/middleware/user.py
+++ server/middleware/user.py
@@ -1,10 +1,9 @@
-import logging
-
 from starlette.authentication import UnauthenticatedUser
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
 from starlette.responses import Response
 
+from server.db.session import create_session_maker
 from server.db.user import fetch_or_create_cached_user
 from server.logger import logger
 from server.types import FirebaseUser
@@ -16,13 +15,15 @@ class UserMiddleware(BaseHTTPMiddleware):
     """
 
     async def dispatch(
-        self, request: Request, call_next: RequestResponseEndpoint
+        self,
+        request: Request,
+        call_next: RequestResponseEndpoint,
     ) -> Response:
         user: FirebaseUser | UnauthenticatedUser = request.scope.get("user")
         if not user.is_authenticated:
             return await call_next(request)
         logger.debug("[%] Found firebase user in request scope", user.identity)
-
-        request.state.user = await fetch_or_create_cached_user(user)
+        async with create_session_maker(request)() as session:
+            request.state.user = await fetch_or_create_cached_user(session, user)
         logger.debug("[%s] Logged in", request.state.user.id)
         return await call_next(request)
