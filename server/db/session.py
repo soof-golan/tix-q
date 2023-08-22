8337259d15e748ef95ad1a35fdba06a7f2f2ae42
Soof Golan <soofgolan@gmail.com>
2023-08-22 14:28:19 +0300

    feat: add db_session dependency

---
+++ server/db/session.py
@@ -0,0 +1,29 @@
+"""
+db Connection utility
+"""
+from typing import Annotated, cast
+
+import fastapi
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+)
+
+
+def _session_maker(request: fastapi.Request) -> async_sessionmaker[AsyncSession]:
+    """
+    Get a database session from the request state
+    """
+    return cast(async_sessionmaker[AsyncSession], request.state.db)
+
+
+async def db_session(
+    session_maker: Annotated[async_sessionmaker[AsyncSession], Depends(_session_maker)]
+) -> AsyncSession:
+    """
+    Get a database session from the request state
+    """
+    async with session_maker() as session:
+        yield session
+
