df6dba41263112345e437298b97912ebd2b842f7
Soof Golan <soofgolan@gmail.com>
2023-08-23 11:39:54 +0300

    fix: use images on public page

--- website/pages/room/@id/index.page.tsx
+++ website/pages/room/@id/index.page.tsx
@@ -8,11 +8,25 @@ export { Page };
 
 type Props = inferProps<typeof onBeforeRender>;
 
-function Page({ id, markdown, title, opensAt, closesAt, ownerEmail }: Props) {
+function Page({
+  id,
+  markdown,
+  title,
+  opensAt,
+  closesAt,
+  ownerEmail,
+  mobileImageBlob,
+  desktopImageBlob,
+}: Props) {
   return (
     <div className="mx-auto max-w-7xl px-2 lg:px-8">
       <div className="mx-auto max-w-3xl">
-        <MarkdownCard content={markdown} title={title} />
+        <MarkdownCard
+          content={markdown}
+          title={title}
+          mobileImageBlob={mobileImageBlob}
+          desktopImageBlob={desktopImageBlob}
+        />
         <WaitingRoomForm
           waitingRoomId={id}
           opensAt={moment(opensAt).toDate()}
