350f5450c776bc5039350b242652a8ae3810ccc2
Soof Golan <soofgolan@gmail.com>
2023-10-03 01:08:32 +0300

    fix: user query caching

--- server/db/user.py
+++ server/db/user.py
@@ -3,6 +3,7 @@ from typing import Annotated
 
 from asyncache import cached
 from cachetools import TTLCache
+from cachetools.keys import methodkey
 from fastapi import Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.dialects.postgresql import insert
@@ -22,6 +23,7 @@ class User(BaseModel):
     email: str
 
 
+@cached(cache=TTLCache(maxsize=1024, ttl=TTL_FIVE_MINUTES), key=methodkey)
 async def fetch_or_create_cached_user(
     session: AsyncSession, user: FirebaseUser
 ) -> User:
@@ -30,26 +32,20 @@ async def fetch_or_create_cached_user(
     This is cached for 5 minutes as user records are immutable
     """
 
-    @cached(cache=TTLCache(maxsize=1024, ttl=TTL_FIVE_MINUTES))
-    async def _cached_fetch_or_create_user(_user: FirebaseUser) -> User:
-        async with session.begin():
-            stmt = (
-                insert(DbUser)
-                .values(firebase_uid=_user.identity, email=_user.email)
-                .on_conflict_do_update(
-                    index_elements=[DbUser.firebase_uid],
-                    set_={DbUser.email: _user.email},
-                )
-                .returning(DbUser.id, DbUser.firebase_uid, DbUser.email)
+    async with session.begin():
+        stmt = (
+            insert(DbUser)
+            .values(firebase_uid=user.identity, email=user.email)
+            .on_conflict_do_update(
+                index_elements=[DbUser.firebase_uid],
+                set_={DbUser.email: user.email},
             )
-            _result = (await session.execute(stmt)).one()
-            await session.commit()
-            result = User(
-                id=_result[0], firebase_uid=_result[1], email=_result[2]
-            )
-        return result
-
-    return await _cached_fetch_or_create_user(user)
+            .returning(DbUser.id, DbUser.firebase_uid, DbUser.email)
+        )
+        _result = (await session.execute(stmt)).one()
+        await session.commit()
+        result = User(id=_result[0], firebase_uid=_result[1], email=_result[2])
+    return result
 
 
 async def authenticated_user(
